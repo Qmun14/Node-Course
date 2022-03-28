e31bad0e64c59416aa12efd02996d32188c8ff06
Qmun14 <mamunramdhan@gmail.com>
2022-03-28 11:42:15 +0700

    Implements to install npm modules from npmjs.com --(module validator) and how to use it

--- notes-app/app.mjs
+++ notes-app/app.mjs
@@ -1,5 +1,22 @@
-import fs from 'fs';
+import notes from './notes.mjs';
+import validator from 'validator';
+
+notes();
+
+console.log(validator.isEmail("mamunramdhan@gmail.com"));
+console.log(validator.isURL("https://qmun14.github.io"));
+
+
+
+
+
+
+
+
+
+
+// import fs from 'fs';
 
 // fs.writeFileSync("./notes.txt", "Hello My Name is Ma'mun");
 
-fs.appendFileSync("./notes.txt", "\r\nHope u enjoy my style cmiiw..");
+// fs.appendFileSync("./notes.txt", "\r\nHope u enjoy my style cmiiw..");
---
+++ notes-app/notes.mjs
@@ -0,0 +1,7 @@
+
+const notes = function getNotes() {
+    return console.log("Hello This is My Notes...!");
+}
+
+export default notes;
+
