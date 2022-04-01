1bfa2395624ae5abbed8c647dce66d6d41db694e
Qmun14 <mamunramdhan@gmail.com>
2022-04-01 11:00:29 +0700

    Customizing The views directory

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,10 +2,42 @@ const path = require('path')
 const express = require('express')
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define path for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates')
+
+// Setup handlebar and views location
+app.set('view engine','hbs')
+app.set('views', viewsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title : 'Weather App',
+        name : "Ma'mun Ramdhan"
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title : 'About Page',
+        name : `Ma'mun Ramdhan`
+    });
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title : 'Help Page',
+        list : {
+            list1 : 'User Guide',
+            list2 : 'Navigation',
+            list3 : 'Contact Administrator'
+        }
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
