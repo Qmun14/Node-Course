c1618bc32954275546f188c9705a89ec17e579a8
Qmun14 <mamunramdhan@gmail.com>
2022-04-03 17:00:24 +0700

    Creating Search form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,23 @@
 console.log(`Client side has on load!`);
 
-fetch('http://localhost:3000/weather?address=').then((response) => {
-    response.json().then((data) => {
-        if (data.err) {
-            return console.log(data.err);
-        } else {
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+const formWeather = document.querySelector('form')
+const search = document.querySelector('input')
+
+formWeather.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const location = search.value;
+
+    fetch(`http://localhost:3000/weather?address=${location.toString()}`).then((response) => {
+        response.json().then((data) => {
+            if (data.err) {
+                return console.log(data.err);
+            } else {
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        })
     })
+
+
 })
