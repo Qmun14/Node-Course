d9e36f46be1b2cfdaba3adef4100b5ad276f7e7c
Qmun14 <mamunramdhan@gmail.com>
2022-04-03 17:43:47 +0700

    Wiring up, setting and styling User Interface (UI) client-side

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,26 @@ console.log(`Client side has on load!`);
 
 const formWeather = document.querySelector('form')
 const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+message1.textContent = 'Location'
+message2.textContent = 'Current Weather'
 
 formWeather.addEventListener('submit', (e) => {
     e.preventDefault();
+    message1.textContent = 'Loading ...'
+    message2.textContent = ''
 
     const location = search.value;
 
     fetch(`http://localhost:3000/weather?address=${location.toString()}`).then((response) => {
         response.json().then((data) => {
             if (data.err) {
-                return console.log(data.err);
+                return message1.textContent = data.err;
             } else {
-                console.log(data.location);
-                console.log(data.forecast);
+                message1.textContent = data.location;
+                message2.textContent =  data.forecast;
             }
         })
     })
