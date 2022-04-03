935be1edb01260f9ff85f631bbd4152f759ca1a7
Qmun14 <mamunramdhan@gmail.com>
2022-04-03 16:28:02 +0700

    Browser HTTP Request with Fetch json promises chaining .then

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,12 @@
 console.log(`Client side has on load!`);
+
+fetch('http://localhost:3000/weather?address=').then((response) => {
+    response.json().then((data) => {
+        if (data.err) {
+            return console.log(data.err);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    })
+})
